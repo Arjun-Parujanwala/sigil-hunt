075421d4ac32c22b1e0b60eed99a6bee779d02eb
Arjun Parujanwala <aparujanwala@gmail.com>
2024-10-31 01:17:48 -0700

    Fixed the centering issue

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,9 +3,12 @@ import './App.css';
 import Opening from '../Opening/Opening';
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Opening />
-      <h1 class='placeholder'>Welcome</h1>
+      <div class='scroll'></div>
+        <h1 class='placeholder'>Welcome...</h1>
+        <h1 class='sigil-hunt'>To Sigil Hunt.</h1>
+      <div />
     </div>
   );
 }
